8ae14f6560c6f657bb28f29479c07bf75fd2d41b
Patrick <lipatrick093@gmail.com>
2024-02-07 03:00:08 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const db = require('./db/db.json')
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend } = require('./fsUtils');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve up static assets from the public folder
+app.use(express.static('public'));
+
+app.get('/api/notes', (req,res)=>{
+    res.json(db)
+});
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.post('/api/notes',(req,res)=>{
+  const {title, text } = req.body;
+
+  if (title && text){
+
+    const newNotes = {
+        title,
+        text,
+        id: uuidv4()
+    }
+    
+  }
+})
+
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
